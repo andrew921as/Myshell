8bc16a33041eeed8c1a3c0aa62db028bbf02184c
valerymolinalika <valery.molina@correounivalle.edu.co>
2022-06-01 22:31:43 -0500

    actualización

---
+++ letra.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <assert.h>
+#include <unistd.h>
+
+int main (){
+    int i;
+    i=1;
+    while (i=1)
+    {
+        printf("A \n");
+        sleep(1);
+    }
+
+    return 0;
+    
+}
--- myshell.c
+++ myshell.c
@@ -4,28 +4,60 @@
 #include <sys/wait.h>
 #include "leercadena.h"
 
-int main(int argc, char* argv[]) {
-    	char command[256];
-    	char **comando;
-		char primera[10];
-		 
-    	while (1) {
-            	printf("> ");
-            	leer_de_teclado(256, command);
-            	if (strcmp(command,"salir") == 0) break;
-            	comando = de_cadena_a_vector(command);		
-				printf( "strlen(comando) = %ld\n", strlen(*comando));
-				printf("%s \n",comando[1]);
-				strncpy(primera,comando[0],strlen(*comando));
-				char *ultimaL[1];
-            	int rc = fork();
-            	assert(rc >= 0);
-            	if (rc == 0)
-                    	execvp(comando[0], comando);
-            	else
-                    	wait(NULL);
-    	}
-    	return 0;
+int main(int argc, char *argv[])
+{
+	char command[256];
+	char **comando;
+	char primera[10];
+	int i;
+	int back;
+	
+	while (1)
+	{
+		printf("> ");
+		leer_de_teclado(256, command);
+		if (strcmp(command, "salir") == 0)
+			break;
+		comando = de_cadena_a_vector(command);
+		//-------------------------------------------------------
+		// Editamos nosotros
+		printf("strlen(comando) = %ld\n", strlen(*comando));
+		printf("%s \n", comando[1]);
+		strncpy(primera, comando[0], strlen(*comando));
+
+		i = 0;
+		while (comando[i])
+		{
+			printf("%s\n", comando[i++]);
+		}
+		//Evaluar si el proceso se corre en segundo plano. 
+		if (strcmp(comando[i - 1], "&") == 0)
+		{
+			comando[i - 1] = NULL;
+			back = 1;
+			
+		}
+		if (strcmp(comando[0], "fg") == 0)
+		{
+			back=0;
+		}
+
+		//-------------------------------------------------------
+		//CREACION DEL HIJO
+		int rc = fork();	
+		assert(rc >= 0);
+		if (rc == 0){
+			printf("soy el hijo num ");
+			execvp(comando[0], comando);
+		}
+		else {
+			//Validamos el proceso en background 
+			if (back != 1)
+			{
+				wait(NULL);
+			}
+		}
+		
+	}
+	return 0;
 }
-
-
